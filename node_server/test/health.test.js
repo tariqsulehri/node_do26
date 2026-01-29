8c57246f9e984c9cfe9ef5cfee8d5d7f810a8cee
tariqsulehri <tariq_sulehri@hotmail.com>
2026-01-29 17:03:42 +0500

    initial commit

---
+++ node_server/test/health.test.js
@@ -0,0 +1,10 @@
+import request from 'supertest';
+import { app } from '../index.js';
+
+describe('GET /health', () => {
+    it('should return 200 OK', async () => {
+        const res = await request(app).get('/health');
+        expect(res.statusCode).toEqual(200);
+        expect(res.text).toEqual('OK');
+    });
+});
